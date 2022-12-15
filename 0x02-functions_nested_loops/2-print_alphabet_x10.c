e19acc09dc8402d4dc10d5435924096dc3e12768
Faith <faith7kawira@gmail.com>
2022-12-15 05:50:54 -0800

    functions

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+
+
+/**
+ * prints _alphabet_*10 - prints the alpha 10 times
+ *
+ * Return: Always 0 (success)
+ */
+
+void print_alphabet_ * 10(void)
+
+{
+
+	int ten;
+
+	char la;
+
+
+
+	for (ten = 0; ten <= '9'; ten++)
+
+	{
+
+		for (la = 'a'; la <= 'z'; la++)
+
+			_putchar(la);
+
+		_putchar('\n');
+
+	}
+
+}
