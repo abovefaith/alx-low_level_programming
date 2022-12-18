a58f2a4e7fd0b3d523740ca047a69ac4b815c23c
Faith <faith7kawira@gmail.com>
2022-12-18 05:30:38 -0800

    0

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+#include <stdlib.h>
+
+#include <time.h>
+
+
+
+/**
+ * main - positive and negative
+ * Return: Always 0
+ */
+
+int positive_or_negative(void)
+
+{
+	int n;
+
+	n = 0;
+	if (n > 0)
+	{
+		printf("%d is positive\n", n);
+	}
+	else if (n == 0)
+	{
+		printf("%d is zero\n", n);
+	}
+	else
+	{
+		printf("%d is negative\n", n);
+	}
+	return (0);
+}
