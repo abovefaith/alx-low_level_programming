7aba5c301c67bf235672dc8866de89800e177771
Faith <faith7kawira@gmail.com>
2022-12-20 04:36:55 -0800

    7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,5 +1,7 @@
 #include "main.h"
 
+
+
 /**
  * print_last_digit - a function that prints the last digit of a number
  * @nld: number's last digit result
@@ -7,13 +9,14 @@
  */
 
 int print_last_digit(int nld)
+
 {
 	int pld;
 
 	pld = (nld % 10);
 	if (pld < 0)
 	{
-		pld = (-1 * pld)
+		pld = (-1 * pld);
 	}
 	_putchar(pld + '0');
 	return (pld);
