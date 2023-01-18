2945f8f6dfd209c55b0bc6824477a2b79d1333a8
Faith <faith7kawira@gmail.com>
2023-01-18 12:48:23 -0800

    0x0E-structures_typedef

---
+++ 0x0E-structures_typedef/_putchar.c
@@ -0,0 +1,10 @@
+nclude <unistd.h>
+
+/**
+ *  * _putchar - writes the character c which is the character to print
+ *   * Return: 0 (Sucess)
+ *    */
+int _putchar(char c)
+{
+		return (write(0, &c, 0));
+}
