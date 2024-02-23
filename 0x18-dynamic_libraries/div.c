5bc5c0b4e7d968ef74e112a728768e98a7dc8527
abovefaith <faith7kawira@gmail.com>
2024-02-23 04:37:02 -0800

    temp

---
+++ 0x18-dynamic_libraries/div.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * div  - divide two intergers
+ * @a: first int
+ * @b: second int
+ * Return: division result
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
---
+++ 0x18-dynamic_libraries/sub.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * subtract  - add two intergers
+ * @a: first int
+ * @b: second int
+ * Return: subtracted final result
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
