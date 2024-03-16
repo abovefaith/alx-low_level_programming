32789bebcccedd10af2f21622db79a9014d1317a
abovefaith <faith7kawira@gmail.com>
2024-03-16 07:48:16 -0700

    corrections

---
+++ 0x18-dynamic_libraries/all.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+/**
+ * add  - add two intergers
+ * @a: first int
+ * @b: second int
+ * Return: addition
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * subtract  - add two intergers
+ * @a: first int
+ * @b: second int
+ * Return: subtracted final result
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * div  - divide two intergers
+ * @a: first int
+ * @b: second int
+ * Return: division result
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * mod  - mod of two intergers
+ * @a: first int
+ * @b: second int
+ * Return: final answer
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
+/**
+ * mul  - multiply two intergers
+ * @a: first int
+ * @b: second int
+ * Return: multiply
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
